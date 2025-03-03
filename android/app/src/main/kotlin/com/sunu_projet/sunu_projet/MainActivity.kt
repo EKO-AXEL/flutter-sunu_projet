b4b1ad12919e855f5938afbbd795b06878ef4d7e
AngeloAxel <angeloaxel13@gmail.com>
2025-03-03 23:34:29 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/sunu_projet/sunu_projet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sunu_projet.sunu_projet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/sunuprojet/sunu_projet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sunuprojet.sunu_projet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
